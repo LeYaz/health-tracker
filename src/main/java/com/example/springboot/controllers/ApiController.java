e8d221e9458eaf03e6b8d0aa950ab7d5c7cac4a3
Yazid <y.aadnan@semeru.fayat.com>
2022-09-30 11:04:56 +0200

    Initial commit

---
+++ src/main/java/com/example/springboot/controllers/ApiController.java
@@ -0,0 +1,12 @@
+package com.example.springboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class ApiController {
+    @GetMapping("/")
+    public String index(){
+        return "Hello bg";
+    }
+}
