35ea803a8b0866defbcda50df11f5c1b94c9c091
Yazid <y.aadnan@semeru.fayat.com>
2022-12-15 15:15:48 +0100

    Dao et Service

--- src/main/java/com/example/springboot/controllers/FoodCompositionController.java
+++ src/main/java/com/example/springboot/controllers/FoodCompositionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FoodCompositionController {
 
     private final ServiceFoodComposition serviceFoodComposition;
+    //test
 
     public FoodCompositionController(ServiceFoodComposition service){
         this.serviceFoodComposition =service;
